9372ca15275e7da61315eeb65586b8e3705f4fd2
beebee-git <blessingnwusulor.u@gmail.com>
2022-06-02 13:37:17 -0700

    c hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main -this function prints a sentence about programming.
+ *
  * return: success (0)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
