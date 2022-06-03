0c64103f95fd5a29c7fa521f96463a8c0937ef57
beebee-git <blessingnwusulor.u@gmail.com>
2022-06-03 15:29:25 -0700

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is main
+ *
+ * Description: this is description
+ * Return: the main return 0
+ */
+int main(void)
+
+	for (char x = "a"; x <= "z"; x++)
+{
+	putchar(x);
+	return (0);
+}
