1f534dab889fe33f1e508a0d19a9ee2427cdef53
beebee-git <blessingnwusulor.u@gmail.com>
2022-06-03 15:59:51 -0700

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: Displays numbers from 0 to 9
+ *
+ * Return: Zero (0)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
