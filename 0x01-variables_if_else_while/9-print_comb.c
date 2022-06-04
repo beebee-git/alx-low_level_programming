2e744ca1ad453a0e42a53a9fa34bb066991652a8
beebee-git <blessingnwusulor.u@gmail.com>
2022-06-04 02:52:29 -0700

    variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,9 @@ int main(void)
 	for (num = '0'; num < 10; num++)
 	{
 		putchar((num % 10) + '0');
-	} if (num < '9')
+	} else if (num < '9')
 	{
-		putchar((',') + ' ');
+		putchar((',') (' '));
 	} else
 	{
 		putchar(' ');
