52c31d945b2204179f6b3d3eb047de1b0ae90d28
beebee-git <blessingnwusulor.u@gmail.com>
2022-06-08 13:05:14 -0700

    my loops commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - This function  prints 10 times the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_alphabet_x10(void);
+{
+	char a, b;
+	
+	for (a = 0; b <= 9; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+	_putchar('\n')
+	}
+}
