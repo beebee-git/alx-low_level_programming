83364afeac944d5091739a7d3fa76f33b890f045
beebee-git <blessingnwusulor.u@gmail.com>
2022-06-10 14:56:16 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This function prints prime factors
+ * @void: no arguement
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long i, number = 612852475143;
+
+	for (i = 2; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
